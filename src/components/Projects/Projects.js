6255af0ee04b5f4ac178eec0f010862e5d3d6f94
ChaitanyaEmani <131893680+ChaitanyaEmani@users.noreply.github.com>
2024-07-04 11:13:31 +0530

    files added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,9 +26,11 @@ function Projects() {
               imgPath={portfolio}
               isBlog={false}
               title="Portfolio"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              description="Portfolio to share my skills and knowledge with recruiters. 
+              Build with react.js, Node.js, express.js and CSS3. 
+              Have sections like about, skills, projects and resume which allows to view my bio."
+              ghLink="https://github.com/ChaitanyaEmani/portfolio"
+              demoLink="https://my-portfolio-theta-rust-28.vercel.app/"
             />
           </Col>
           
